85707c44c63b7bd300c84bfe2dd4a0d910ffe186
robotic7o7 <scientificrohan@gmail.com>
2021-07-29 22:37:11 +0530

    latest commit

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+const cors = require("cors");
+var dotenv = require("dotenv");
+
+dotenv.config();
+
+
+//mongoose
+var mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+};
+
+mongoose.connect(
+    `mongodb+srv://robotic707:${process.env.MONGOPWD}@cluster0.msywl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    mongooseOptions
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("mongodb connection established");
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
